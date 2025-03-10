95631daee6fd900e897d51fd2d5db9f4b4ac065d
TsyselskyiAndrey <106165881+TsyselskyiAndrey@users.noreply.github.com>
2025-03-10 14:05:33 +0100

    Some minor changes to check if everything works

--- back-end/SocialMediaGloweeServer/SocialMediaGloweeServer/Program.cs
+++ back-end/SocialMediaGloweeServer/SocialMediaGloweeServer/Program.cs
@@ -7,7 +7,7 @@ namespace SocialMediaGloweeServer
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello World, hello developers!");
 
             app.Run();
         }
