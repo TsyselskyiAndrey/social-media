d7c26fef6092111bb1b5639fd72394adadb8e513
TsyselskyiAndrey <106165881+TsyselskyiAndrey@users.noreply.github.com>
2025-03-10 12:55:42 +0100

    The server has been created

---
+++ back-end/SocialMediaGloweeServer/SocialMediaGloweeServer/Program.cs
@@ -0,0 +1,15 @@
+namespace SocialMediaGloweeServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
