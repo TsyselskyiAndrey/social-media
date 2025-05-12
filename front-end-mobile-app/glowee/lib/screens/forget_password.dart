997bba042dd1e4afc270c43c3fbaa11fc277fb45
Kate Alekseeva <kate.alekseeva@appliedmethods.biz>
2025-05-12 19:02:55 +0300

    add new screen forget password

---
+++ front-end-mobile-app/glowee/lib/screens/forget_password.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glowee/screens/login_screen.dart';
+import 'package:glowee/screens/register.dart';
+import 'package:glowee/screens/email_password_text_comfirmation_screen.dart';
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final passwordConfirme = TextEditingController();
+  final username = TextEditingController();
+  final bio = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final email_F = FocusNode();
+  final password_F = FocusNode();
+  final passwordConfirme_F = FocusNode();
+  final username_F = FocusNode();
+  final bio_F = FocusNode();
+
+  @override
+  void dispose() {
+    email.dispose();
+    password.dispose();
+    passwordConfirme.dispose();
+    username.dispose();
+    bio.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(17, 140, 140, 1.0),
+              Color.fromRGBO(38, 75, 198, 1.0),
+              Color.fromRGBO(242, 188, 23, 1.0)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: SafeArea(
+          child: Form(
+            key:_formKey,
+            child: Column(
+              children: [
+                SizedBox(height: 20.h),
+                Center(child: Image.asset('assets/images/logo.png')),
+                SizedBox(height: 30.h),
+                ResetPasswordText(),
+                SizedBox(height: 35.h),
+                SizedBox(height: 25.h),
+                Textfild(
+                  password,
+                  password_F,
+                  'New Password',
+                  Icons.lock,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Password is required';
+                    if (value.length < 6) return 'Password must be at least 6 characters';
+                    if (!RegExp(r'[A-Z]').hasMatch(value)) return 'Password must contain an uppercase letter';
+                    if (!RegExp(r'[0-9]').hasMatch(value)) return 'Password must contain a number';
+                    if (RegExp(r'[ @?,*^]').hasMatch(value)) return 'Password contains invalid characters';
+                    return null;
+                  },
+                ),
+                SizedBox(height: 25.h),
+                Textfild(
+                    passwordConfirme,
+                    passwordConfirme_F,
+                    'Confirm Password',
+                    Icons.lock_outline,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) return 'Please confirm your password';
+                      if (value != password.text) return 'Passwords do not match';
+                      return null;
+                    }
+                ),
+                SizedBox(height: 20.h),
+                ChangePasswordBtn(),
+                SizedBox(height: 15.h),
+                BackBtn(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget ResetPasswordText() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 35.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Enter your new password",
+            style: TextStyle(fontSize: 24.sp, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget BackBtn() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  LoginScreen()),
+              );
+            },
+            child: Text(
+              "Back",
+              style: TextStyle(fontSize: 15.sp, color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget ChangePasswordBtn() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: InkWell(
+        onTap: () {
+          if (_formKey.currentState!.validate()) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) =>  EmailPasswordConfirmationScreen()),
+            );
+          }
+        },
+        child: Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 44.h,
+          decoration: BoxDecoration(
+            color: Colors.blue.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(10.r),
+            border: Border.all(color: Colors.black, width: 1.w),
+          ),
+          child: Text(
+            'Change password',
+            style: TextStyle(fontSize: 23.sp, color: Colors.black),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+
+  Padding Textfild(
+      TextEditingController controll,
+      FocusNode focusNode,
+      String typename,
+      IconData icon, {
+        String? Function(String?)? validator,
+      }) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: TextFormField(
+        style: TextStyle(fontSize: 18.sp, color: Colors.black),
+        controller: controll,
+        focusNode: focusNode,
+        validator: validator,
+        decoration: InputDecoration(
+          hintText: typename,
+          prefixIcon: Icon(icon, color: focusNode.hasFocus ? Colors.black : Colors.grey[600]),
+          contentPadding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(width: 1.w, color: Colors.black),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(width: 2.w, color: Colors.black),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(width: 1.5.w, color: Colors.red),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5.r),
+            borderSide: BorderSide(width: 2.w, color: Colors.red),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+}
