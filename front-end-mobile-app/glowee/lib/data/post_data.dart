bf91c404f3fa4f6c428e672e42c95a4d5f472f56
Kate Alekseeva <kate.alekseeva@appliedmethods.biz>
2025-05-23 09:09:19 +0300

    add post screen

---
+++ front-end-mobile-app/glowee/lib/data/post_data.dart
@@ -0,0 +1,8 @@
+import 'package:glowee/model/post_model.dart';
+import 'package:flutter/material.dart';
+
+List<Post> posts = [];
+final nameNotifier = ValueNotifier<String>('Software Engineering');
+final usernameNotifier = ValueNotifier<String>('software_nure');
+final bioNotifier = ValueNotifier<String>('Why this task has 5 scores???');
+final birthdayNotifier = ValueNotifier<String>('01.01.2020');
