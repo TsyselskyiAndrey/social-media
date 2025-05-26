97d57e35f796d9f71b63977b7b0bcbd563cfec8b
Kate Alekseeva <kate.alekseeva@appliedmethods.biz>
2025-05-26 09:58:37 +0300

    add post data

--- front-end-mobile-app/glowee/lib/data/post_data.dart
+++ front-end-mobile-app/glowee/lib/data/post_data.dart
@@ -1,7 +1,14 @@
 import 'package:glowee/model/post_model.dart';
 import 'package:flutter/material.dart';
 
-List<Post> posts = [];
+final ValueNotifier<List<Post>> postsNotifier = ValueNotifier<List<Post>>([]);
+List<Post> posts = []; // Your existing posts list
+
+// Function to add a post and notify listeners
+void addPost(Post post) {
+  posts.add(post);
+  postsNotifier.value = List.from(posts);
+}
 final nameNotifier = ValueNotifier<String>('Software Engineering');
 final usernameNotifier = ValueNotifier<String>('software_nure');
 final bioNotifier = ValueNotifier<String>('Why this task has 5 scores???');
