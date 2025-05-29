ba2743829afa2f372579a3e3cf0765f251faa0a1
KateFedkova <kate.fedkova@gmail.com>
2025-05-29 15:19:19 +0300

    Add bloc to RegisterDetails

--- front-end-mobile-app/glowee/lib/screens/register_details.dart
+++ front-end-mobile-app/glowee/lib/screens/register_details.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:glowee/bloc/auth_bloc/auth_bloc.dart';
+import 'package:glowee/bloc/auth_bloc/auth_events.dart';
+import 'package:glowee/bloc/auth_bloc/auth_states.dart';
 import 'package:glowee/screens/login_screen.dart';
+import 'package:glowee/screens/otp_verification.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:glowee/screens/email_enter.dart';
@@ -17,7 +22,8 @@ class _RegisterDetailsState extends State<RegisterDetails> {
   // Контролери для текстових полів
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
 
@@ -33,6 +39,9 @@ class _RegisterDetailsState extends State<RegisterDetails> {
 
   // Змінна для збереження обраного зображення
   File? _imageFile;
+
+  bool isPhotoSent = false;
+  bool isInfoSent = false;
 
   @override
   void initState() {
@@ -61,11 +70,15 @@ class _RegisterDetailsState extends State<RegisterDetails> {
 
   // Метод для вибору зображення з галереї
   Future<void> _pickImage() async {
-    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
         _imageFile = File(pickedFile.path);
       });
+      context
+          .read<AuthBloc>()
+          .add(AddPhotoWhileSignUpBtnClicked(file: _imageFile!));
     }
   }
 
@@ -120,100 +133,131 @@ class _RegisterDetailsState extends State<RegisterDetails> {
   }
 
   // Метод для обробки події реєстрації
-  void _handleRegistration() {
+  void _handleRegistration(BuildContext context) {
     if (_formKey.currentState!.validate()) {
       // Якщо форма валідна, продовжуємо
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => EmailEnter(emailText: _emailController.text),
-        ),
-      );
+      context.read<AuthBloc>().add(
+            Register2BtnClicked(
+              firstName: _usernameController.text,
+              lastName: _passwordController.text,
+              birthDate: _confirmPasswordController.text,
+            ),
+          );
+      // Navigator.push(
+      //   context,
+      //   MaterialPageRoute(
+      //     builder: (context) => EmailEnter(emailText: _emailController.text),
+      //   ),
+      // );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromRGBO(27, 36, 136, 0.7019607843137254),
-                Color.fromRGBO(242, 188, 23, 0.5)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            )),
-        child: SafeArea(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                SizedBox(height: 20.h),
-                Center(child: Image.asset('assets/images/logo.png')),
-                SizedBox(height: 10.h),
-                InkWell(
-                  onTap: _pickImage,
-                  child: CircleAvatar(
-                    radius: 36.r,
-                    backgroundColor: Colors.grey,
-                    child: _imageFile == null
-                        ? CircleAvatar(
-                      radius: 34.r,
-                      backgroundImage: AssetImage('assets/images/stars.png'),
-                      backgroundColor: Colors.grey.shade200,
-                    )
-                        : CircleAvatar(
-                      radius: 34.r,
-                      backgroundImage: FileImage(_imageFile!),
-                      backgroundColor: Colors.grey.shade200,
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state is AuthStepSucess &&
+            isPhotoSent &&
+            state.flow == AuthFlow.RegisterStep2) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => BlocProvider(
+                create: (context) => AuthBloc(),
+                child: OtpVerificationScreen(),
+              ),
+            ),
+          );
+        } else if (state is AuthStepSucess &&
+            state.flow == AuthFlow.RegisterStep2) {
+          isInfoSent = true;
+        } else if (state is AuthStepSucess &&
+            state.flow == AuthFlow.UploadPhoto) {
+          isPhotoSent = true;
+        }
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(27, 36, 136, 0.7019607843137254),
+              Color.fromRGBO(242, 188, 23, 0.5)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: SafeArea(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 20.h),
+                  Center(child: Image.asset('assets/images/logo.png')),
+                  SizedBox(height: 10.h),
+                  InkWell(
+                    onTap: _pickImage,
+                    child: CircleAvatar(
+                      radius: 36.r,
+                      backgroundColor: Colors.grey,
+                      child: _imageFile == null
+                          ? CircleAvatar(
+                              radius: 34.r,
+                              backgroundImage:
+                                  AssetImage('assets/images/stars.png'),
+                              backgroundColor: Colors.grey.shade200,
+                            )
+                          : CircleAvatar(
+                              radius: 34.r,
+                              backgroundImage: FileImage(_imageFile!),
+                              backgroundColor: Colors.grey.shade200,
+                            ),
                     ),
                   ),
-                ),
-                _createAccountText(),
-                SizedBox(height: 25.h),
-                SizedBox(height: 5.h),
-                _buildTextField(
-                  controller: _usernameController,
-                  focusNode: _usernameFocusNode,
-                  label: 'Username',
-                  icon: Icons.person,
-                  validator: _validateUsername,
-                ),
-                SizedBox(height: 5.h),
-                _buildTextField(
-                  controller: _emailController,
-                  focusNode: _emailFocusNode,
-                  label: 'Email',
-                  icon: Icons.email,
-                  validator: _validateEmail,
-                ),
-                SizedBox(height: 5.h),
-                _buildTextField(
-                  controller: _passwordController,
-                  focusNode: _passwordFocusNode,
-                  label: 'Password',
-                  icon: Icons.lock,
-                  obscureText: true,
-                  validator: _validatePassword,
-                ),
-                SizedBox(height: 5.h),
-                _buildTextField(
-                  controller: _confirmPasswordController,
-                  focusNode: _confirmPasswordFocusNode,
-                  label: 'Confirm Password',
-                  icon: Icons.lock_outline,
-                  obscureText: true,
-                  validator: _validateConfirmPassword,
-                ),
-                SizedBox(height: 20.h),
-                _buildNextButton(),
-                SizedBox(height: 15.h),
-                _buildLoginLink(),
-              ],
+                  _createAccountText(),
+                  SizedBox(height: 25.h),
+                  SizedBox(height: 5.h),
+                  _buildTextField(
+                    controller: _usernameController,
+                    focusNode: _usernameFocusNode,
+                    label: 'Username',
+                    icon: Icons.person,
+                    validator: _validateUsername,
+                  ),
+                  SizedBox(height: 5.h),
+                  _buildTextField(
+                    controller: _emailController,
+                    focusNode: _emailFocusNode,
+                    label: 'Email',
+                    icon: Icons.email,
+                    validator: _validateEmail,
+                  ),
+                  SizedBox(height: 5.h),
+                  _buildTextField(
+                    controller: _passwordController,
+                    focusNode: _passwordFocusNode,
+                    label: 'Password',
+                    icon: Icons.lock,
+                    obscureText: true,
+                    validator: _validatePassword,
+                  ),
+                  SizedBox(height: 5.h),
+                  _buildTextField(
+                    controller: _confirmPasswordController,
+                    focusNode: _confirmPasswordFocusNode,
+                    label: 'Confirm Password',
+                    icon: Icons.lock_outline,
+                    obscureText: true,
+                    validator: _validateConfirmPassword,
+                  ),
+                  SizedBox(height: 20.h),
+                  _buildNextButton(),
+                  SizedBox(height: 15.h),
+                  _buildLoginLink(),
+                ],
+              ),
             ),
           ),
         ),
@@ -273,7 +317,7 @@ class _RegisterDetailsState extends State<RegisterDetails> {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.w),
       child: InkWell(
-        onTap: _handleRegistration,
+        onTap: () => _handleRegistration(context),
         child: Container(
           alignment: Alignment.center,
           width: double.infinity,
@@ -314,7 +358,7 @@ class _RegisterDetailsState extends State<RegisterDetails> {
           prefixIcon: Icon(icon,
               color: focusNode.hasFocus ? Colors.black : Colors.grey[600]),
           contentPadding:
-          EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+              EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
           filled: true,
           fillColor: Colors.white,
           enabledBorder: OutlineInputBorder(
