8bc3c2b365aa827cd13583e9f97bfdddae811488
KateFedkova <kate.fedkova@gmail.com>
2025-06-03 00:01:41 +0300

    Add PostState

---
+++ front-end-mobile-app/glowee/lib/bloc/post_bloc/post_states.dart
@@ -0,0 +1,25 @@
+import 'package:glowee/model/post.dart';
+
+abstract class PostState {}
+
+class PostLoading extends PostState {}
+
+class PostLoaded extends PostState {
+  final List<Post> posts;
+
+  PostLoaded({required this.posts});
+
+  PostLoaded copyWith({required posts}) {
+    return PostLoaded(posts: posts);
+  }
+}
+
+class PostStepSuccess extends PostState {
+  PostStepSuccess();
+}
+
+class PostError extends PostState {
+  final List<String> messages;
+
+  PostError({required this.messages});
+}
