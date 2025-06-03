0d87ffec4980ffee75cf923f7ef0f23cf3828f68
KateFedkova <kate.fedkova@gmail.com>
2025-06-03 20:33:46 +0300

    Add Tag model

---
+++ front-end-mobile-app/glowee/lib/model/tag.dart
@@ -0,0 +1,13 @@
+class Tag {
+  final int id;
+  final String name;
+
+  Tag({required this.id, required this.name});
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+  }
+}
